9687fbb3fdd9aca9eb175d20ba375048212b57d9
Z-Mengchu <mj686866@163.com>
2024-02-29 16:35:07 +0800

    更改.gitignore内容

---
+++ src/main/java/com/mengchu/config/ThreadPoolConfig.java
@@ -0,0 +1,22 @@
+package com.mengchu.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.*;
+
+@Configuration
+public class ThreadPoolConfig {
+    @Bean
+    public ExecutorService threadPoolExecutor(){
+        return new ThreadPoolExecutor(
+                3,//核心线程数量
+                17,//最大线程数量
+                20,//空闲线程最大存活时间
+                TimeUnit.MINUTES,//时间单位
+                new ArrayBlockingQueue<>(2),//任务队列
+                Executors.defaultThreadFactory(),//创建线程工厂
+                new ThreadPoolExecutor.AbortPolicy()//任务的拒绝策略
+        );
+    }
+}
