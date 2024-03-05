b713b2e7f1b4291324c0b56d8dff8e397f1027c7
Z-Mengchu <mj686866@163.com>
2024-03-05 12:48:08 +0800

    更改.gitignore内容

--- src/main/java/com/mengchu/config/WebConfig.java
+++ src/main/java/com/mengchu/config/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -28,12 +29,12 @@ public class WebConfig implements WebMvcConfigurer {
     }
 
     //设置跨域请求
-    /*@Override
+    @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // 所有的当前站点的请求地址，都支持跨域访问。
                 .allowedOrigins("*")  // 所有的外部域都可以跨域访问。如果是localhost形式的域名,需要设置为"localhost"
                 .allowCredentials(true)  // 是否支持跨域用户凭证
-                .allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH")  // 当前站点支持的跨域请求类型是什么
+                .allowedMethods("GET", "POST", "DELETE", "PUT")  // 当前站点支持的跨域请求类型是什么
                 .maxAge(3600);  // 预检请求的有效期，单位为秒。
-    }*/
+    }
 }
